27928fab0c537f0e173f6bee6d34be1866c75a92
LyokoJames <jcr972000@yahoo.com.au>
2013-08-04 18:36:24 +1000

    Adding mcmod.info and ant build files

--- rpPlaceholder_common/rpph/lib/Reference.java
+++ rpPlaceholder_common/rpph/lib/Reference.java
@@ -3,7 +3,7 @@ package rpph.lib;
 public class Reference {
     public static final String MOD_ID = "rpPlaceholder";
     public static final String MOD_NAME = "RedPower Placeholder Mod";
-    public static final String VERSION = "0.1.0";
+    public static final String VERSION = "1.0.0";
     public static final String CHANNEL_NAME = MOD_ID;
     
     public static final String CLIENT_PROXY_CLASS = "rpph.core.proxy.ClientProxy";
